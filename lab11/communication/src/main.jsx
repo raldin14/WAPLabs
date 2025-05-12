ab8fe05787d70ab50af7d0f72abdb95c0ff09c55
raldin14 <raldin.hidalgo@gmail.com>
2025-05-12 17:01:13 -0500

    Fixing link'

--- lab11/communication/src/main.jsx
+++ lab11/communication/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BookProvider } from './context/BookContext.jsx'
-import {BrowserRouter} from 'react-router';
+import {HashRouter} from 'react-router';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <BookProvider>
         <App/>
       </BookProvider>
-    </BrowserRouter>    
+    </HashRouter>    
   </StrictMode>,
 )
